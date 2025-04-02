1fb2ff5629f1eeac30a4f1186fdc6d133b9e828a
nancysilva <A00833627@tec.mx>
2025-04-01 18:57:29 -0600

    Cambios Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// CAMBIA esto por el nombre exacto de tu repositorio de GitHub
 export default defineConfig({
-  plugins: [react()]
-});
+  base: '/Practica-GITHUB-ACTIONS/', 
+  plugins: [react()],
+})
