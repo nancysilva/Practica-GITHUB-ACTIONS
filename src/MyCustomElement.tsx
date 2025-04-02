baeeb94ce94f0ea93f4e00da2db0a228be33f0ff
nancysilva <A00833627@tec.mx>
2025-04-01 19:40:51 -0600

    Cambio Nombre

--- src/MyCustomElement.tsx
+++ src/MyCustomElement.tsx
@@ -10,7 +10,7 @@ export const MyCustomElement = () => {
           fontSize: ThemingParameters.sapFontHeader1Size,
         }}
       >
-        My custom Text Element
+        Nancy Silva Alvarez
       </span>
     </div>
   );
